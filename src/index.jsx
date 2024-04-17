98c10e868186355d80dbac211659297319585d83
coderzmy <principalming@outlook.com>
2024-04-18 01:35:56 +0800

    需求一:基于数据值，判断元素的显隐

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/index.less';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-fetch('/jian/subscriptions/recommended_collections')
-  .then(res => res.json())
-  .then(res => {
-    console.log(res);
-  });
+let flag = false;
+let isRun = false;
 root.render(
   <React.StrictMode>
     <div>数字浙江</div>
+    <br />
+    <button
+      style={{
+        display: flag ? 'block' : 'none',
+      }}
+    >
+      按钮1
+    </button>
+    <br />
+    {isRun ? <button>按钮2</button> : false}
   </React.StrictMode>
 );
 
